6f13f31a22cc28dfe4103f22255a129c65f5e3f5
b4lance <leonciorequena1995@gmail.com>
2019-03-20 00:34:28 -0430

    Error Solvented

--- php/pacientes/detectarcedula.php
+++ php/pacientes/detectarcedula.php
@@ -2,17 +2,16 @@
 sleep(1);
 require("../../config/conexion.php");
 if($_REQUEST) {
-    $ced = $_REQUEST['cedula'];
+    $cedula = $_REQUEST['cedula'];
     $nacionalidad=$_REQUEST['nacionalidad'];
-    $cedula=$nacionalidad.'-'.$ced;
-    $query = "SELECT * FROM pacientes WHERE ced_paciente = '".strtolower($cedula)."'";
+    $query = "SELECT * FROM pacientes WHERE nac_paciente='$nacionalidad' AND ced_paciente='$cedula'";
     $results = $con->query($query);
 
     if($results->num_rows > 0){
-        echo "<script>swal('La Cedula ingresada ya existe','','error');</script>";
+        echo "<script>swal('La cedula ingresada ya existe','','error');</script>";
     	return false;
     }else{
-          echo "<script>swal('La Cedula Ingresada esta disponible','','success');";
+          echo "<script>swal('La cedula Ingresada esta disponible','','success');";
           return false;
       }
 }
